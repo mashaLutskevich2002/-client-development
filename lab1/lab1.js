e0efe412e02f6a60dbee9c3542bdf86f34c2e1c2
mashaLutskevich2002 <masha.lutskevich2002@gmail.com>
2020-09-08 13:17:36 +0300

    first commit

---
+++ lab1/lab1.js
@@ -0,0 +1,39 @@
+//task1
+function numberToArr(x){
+    let arr = []
+    for(i = 0; i < x; i++){
+        arr.push(i)
+    }
+   return arr
+}
+
+console.log( numberToArr(10))
+
+
+//task2
+function division(a,b,c){
+    if (a % b == 0  && a % c == 0 ){
+        return true
+    }
+    else {
+        return false
+    }        
+}
+console.log(division(90,10,5))
+
+
+
+//task3
+function myCapitalize(string){
+    if(string.length<=10){
+        return string.charAt(0).toUpperCase() + string.slice(1)
+    }
+    else{
+        return("Ошибка. Слово не должно иметь более 10 символов!")
+    }
+}
+    console.log(myCapitalize("мария"))
+    
+
+
+
---
+++ lab1/new.js
@@ -0,0 +1,8 @@
+ document.body.style.background = 'gray'
+ document.body.style.textAlign = 'center'
+
+function f1(){
+   let x =  document.getElementById('inp1').value 
+     document.getElementById('div1').innerHTML = x
+   
+}
